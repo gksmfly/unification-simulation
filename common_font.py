8fdaab3794695dc0227ff657dbb49f0d5dcdef64
하늘 <149565853+gksmfly@users.noreply.github.com>
2025-08-05 13:35:59 +0900

    Update common_font.py

--- common_font.py
+++ common_font.py
@@ -1,11 +1,19 @@
+# common_font.py
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 import os
 
-# 1. 폰트 파일 경로 지정 (Python 파일과 같은 위치에 있는 경우)
-FONT_PATH = os.path.join(os.path.dirname(__file__), "NanumGothic.ttf")
-fontprop = fm.FontProperties(fname=FONT_PATH)
+def set_korean_font():
+    """
+    Matplotlib 그래프에서 한글이 깨지지 않도록
+    NanumGothic.ttf 폰트를 적용하는 함수
+    """
+    FONT_PATH = os.path.join(os.path.dirname(__file__), "NanumGothic.ttf")
 
-# 2. Matplotlib 전역 폰트 설정
-plt.rcParams['font.family'] = fontprop.get_name()
-plt.rcParams['axes.unicode_minus'] = False
+    # 폰트 파일이 실제로 존재하는지 확인
+    if not os.path.exists(FONT_PATH):
+        raise FileNotFoundError(f"폰트 파일을 찾을 수 없습니다: {FONT_PATH}")
+
+    fontprop = fm.FontProperties(fname=FONT_PATH)
+    plt.rcParams['font.family'] = fontprop.get_name()
+    plt.rcParams['axes.unicode_minus'] = False
